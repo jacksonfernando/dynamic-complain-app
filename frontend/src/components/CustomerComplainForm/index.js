5e143e84e8265e4e35cc52858ac748f8177540e4
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-28 21:18:13 +0700

    change background color of global css

--- frontend/src/components/CustomerComplainForm/index.js
+++ frontend/src/components/CustomerComplainForm/index.js
@@ -7,6 +7,7 @@ import TextArea from "../TextArea";
 import Label from "../Label";
 import Dropdown from "../Dropdown";
 import ExtraFields from "./ExtraFields";
+import Button from "../Button";
 
 const CustomerComplainhtmlForm = () => {
   const [fetchedCategories, setFetchedCategories] = useState([]);
@@ -20,56 +21,68 @@ const CustomerComplainhtmlForm = () => {
     const categories = fetchedCategories
       .find((category) => category.id === parseInt(event.target.value))
     categories.key = extraFields.length;
-    console.log(categories)
     setExtraFields(prev => [...prev, categories]);
   }
+
+  const renderFullNameSection = () => (
+    <>
+      <div className="col-span-full">
+        <Label name={"Full name"} />
+        <div className="mt-2">
+          <TextInput autoComplete={"full-name"} name={"full-name"} id="full-name" inputType={"text"} />
+        </div>
+      </div>
+    </>
+  )
+
+  const renderEmailSection = () => (
+    <div className="col-span-full">
+      <Label name={"Email"} />
+      <div className="mt-2">
+        <TextInput autoComplete={"full-name"} name={"full-name"} id="full-name" inputType={"email"} />
+      </div>
+    </div>
+  )
+
+  const renderCategoriesSection = () => (
+    <div className="col-span-full">
+      <Label name={"Category"} />
+      <div className="mt-2">
+        <Dropdown
+          name={"category"}
+          id={"category"}
+          onChangeEvent={onChangeCategories}
+          options={CATEGORIES}
+        />
+      </div>
+    </div>
+  )
+
+  const renderIssueDescriptionSection = () => (
+    <div className="col-span-full">
+      <Label name={"Issue Desccription"} />
+      <div className="mt-2">
+        <TextArea id={"issue"} name={"issue"} />
+      </div>
+      <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about the issue.</p>
+    </div>
+  )
 
   return (
     <form>
       <div className="border-b border-gray-900/10 pb-12">
         <h2 className="text-base font-semibold leading-7 text-gray-900">Complain Form</h2>
         <p className="mt-1 text-sm leading-6 text-gray-600">Submit your complain here</p>
-
         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="col-span-full">
-            <Label name={"Full name"} />
-            <div className="mt-2">
-              <TextInput autoComplete={"full-name"} name={"full-name"} id="full-name" inputType={"text"} />
-            </div>
-          </div>
-
-          <div className="col-span-full">
-            <Label name={"Email"} />
-            <div className="mt-2">
-              <TextInput autoComplete={"full-name"} name={"full-name"} id="full-name" inputType={"email"} />
-            </div>
-          </div>
-
-          <div className="col-span-full">
-            <Label name={"Category"} />
-            <div className="mt-2">
-              <Dropdown
-                name={"category"}
-                id={"category"}
-                onChangeEvent={onChangeCategories}
-                options={CATEGORIES}
-              />
-            </div>
-          </div>
-
-          <div className="col-span-full">
-            <Label name={"Issue Desccription"} />
-            <div className="mt-2">
-              <TextArea id={"issue"} name={"issue"} />
-            </div>
-            <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about the issue.</p>
-          </div>
+          {renderFullNameSection()}
+          {renderEmailSection()}
+          {renderCategoriesSection()}
+          {renderIssueDescriptionSection()}
           <ExtraFields extraFields={extraFields} />
         </div>
-
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-          <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-        </div>
+      </div>
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <Button text={'save'} type={'submit'} />
       </div>
     </form >
   )
