c87d65d2ec16ea814e10ea3950d4e73f4e846c9f
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-29 21:40:13 +0700

    set close modalc

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,15 +1,20 @@
+'use client';
+
 import CustomerComplainForm from "@/components/CustomerComplainForm";
 import Modal from "@/components/Modal";
 import Navbar from "@/components/Navbar";
+import { useState } from "react";
 
 export default function Home() {
+  const [signInModalOpen, setSignInModalOpen] = useState(true);
+
   return (
     <>
       <Navbar />
       <div className="mx-auto my-16 max-w-3xl">
         <CustomerComplainForm />
       </div>
-      <Modal open={true} />
+      <Modal open={signInModalOpen} setOpen={setSignInModalOpen} />
     </>
   )
 }
