9df7e0a857c347524fe6dc0705fb02fbc5bc1a03
jacksonfernando <jacksonfernando51@gmail.com>
2023-09-30 21:09:49 +0700

    add complain object and repository

--- backend/src/main/java/com/example/complain/entity/Complain.java
+++ backend/src/main/java/com/example/complain/entity/Complain.java
@@ -27,6 +27,63 @@ public class Complain {
     @Column(name = "email", nullable = false)
     private String email;
 
+    @Nonnull
+    @Column(name = "issue_description", nullable = false)
+    private String issueDescription;
+
     @JdbcTypeCode(SqlTypes.JSON)
     private List<Map<String, String>> extraFields;
+
+    public Complain(long id, String fullName, String email, String issueDescription,
+            List<Map<String, String>> extraFields) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.issueDescription = issueDescription;
+        this.extraFields = extraFields;
+    }
+
+    public Complain() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIssueDescription() {
+        return issueDescription;
+    }
+
+    public void setIssueDescription(String issueDescription) {
+        this.issueDescription = issueDescription;
+    }
+
+    public List<Map<String, String>> getExtraFields() {
+        return extraFields;
+    }
+
+    public void setExtraFields(List<Map<String, String>> extraFields) {
+        this.extraFields = extraFields;
+    }
+
 }
--- backend/src/main/java/com/example/complain/repository/ComplainRepository.java
+++ backend/src/main/java/com/example/complain/repository/ComplainRepository.java
@@ -2,11 +2,11 @@ package com.example.complain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.complain.entity.Category;
+import com.example.complain.entity.Complain;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface ComplainRepository extends JpaRepository<Category, Long> {
-    Page<Category> findAll(Pageable pageable);
+public interface ComplainRepository extends JpaRepository<Complain, Long> {
+    Page<Complain> findAll(Pageable pageable);
 }
