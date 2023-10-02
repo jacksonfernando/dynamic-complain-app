2c13871f78b34a44e4f6764886dd1ea0a7341920
jacksonfernando <jacksonfernando51@gmail.com>
2023-10-02 21:58:52 +0700

    refactor content

--- frontend/src/app/admin/page.js
+++ frontend/src/app/admin/page.js
@@ -1,49 +1,69 @@
+import Sidebar from "@/components/Sidebar";
+
 
 const Page = () => {
+  const renderHeading = () => {
+    const headingsLabel = ['Label', 'Type', 'Value', 'Action']
+    return (
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" className="px-6 py-3">
+            label
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Type
+          </th>
+          <th scope="col" className="px-6 py-3">
+            value
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Price
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Action
+          </th>
+        </tr>
+      </thead>
+
+    )
+  }
+
+  const renderContent = () => {
+    const headingsLabel = ['Label', 'Type', 'Value', 'Action']
+    return (
+      <tbody>
+        <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+          <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            Apple MacBook Pro 17"
+          </th>
+          <td className="px-6 py-4">
+            Silver
+          </td>
+          <td className="px-6 py-4">
+            Laptop
+          </td>
+          <td className="px-6 py-4">
+            $2999
+          </td>
+          <td className="px-6 py-4">
+            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+          </td>
+        </tr>
+      </tbody>
+    )
+  }
+
   return (
     <>
-      <button data-drawer-target="sidebar-multi-level-sidebar" data-drawer-toggle="sidebar-multi-level-sidebar" aria-controls="sidebar-multi-level-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-        <span class="sr-only">Open sidebar</span>
-        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-        </svg>
-      </button>
-
-      <aside id="sidebar-multi-level-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-        <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-          <ul class="space-y-2 font-medium">
-            <li>
-              <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                <span class="flex-1 ml-3 text-left whitespace-nowrap">E-commerce</span>
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                </svg>
-              </button>
-              <ul id="dropdown-example" class="hidden py-2 space-y-2">
-                <li>
-                  <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Products</a>
-                </li>
-                <li>
-                  <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Billing</a>
-                </li>
-                <li>
-                  <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <span class="flex-1 ml-3 whitespace-nowrap">Products</span>
-              </a>
-            </li>
-          </ul>
+      <Sidebar />
+      <div className="p-4 sm:ml-64">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            {renderHeading()}
+            {renderContent()}
+          </table>
         </div>
-      </aside>
+      </div>
     </>
   )
 }
---
+++ frontend/src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+const Sidebar = () => {
+  return (
+    <aside id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+      <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <ul className="space-y-2 font-medium">
+          <li>
+            <button type="button" className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+              <span className="flex-1 ml-3 text-left whitespace-nowrap">Categories</span>
+              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+              </svg>
+            </button>
+          </li>
+          <li>
+            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <span className="flex-1 ml-3 whitespace-nowrap">Complains</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
+
