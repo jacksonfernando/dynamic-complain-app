e9f5882a26c484790ad8f9295bb981de1c753394
jacksonfernando <jacksonfernando51@gmail.com>
2023-10-03 20:45:05 +0700

    style dropdown

--- frontend/src/components/Modal/CategoryModal.js
+++ frontend/src/components/Modal/CategoryModal.js
@@ -84,8 +84,7 @@ const CategoryModal = ({ open, setOpen, defaultValues }) => {
                   {renderErrorText(errors.type, 'Type is required')}
                 </div>
                 {
-                  <div className='mt-2'>
-                    <Label name={'Value'} />
+                  <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
                     <ExtraFieldsCategories
                       fields={fields}
                       register={register}
