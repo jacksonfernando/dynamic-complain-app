ea4be437810a7c2f40897ab6ee34fa3eda552122
jacksonfernando <jacksonfernando51@gmail.com>
2023-10-11 23:19:02 +0700

    add category test case

--- backend/src/test/com/example/complain/controller/CategoryControllerTest.java
+++ backend/src/test/com/example/complain/controller/CategoryControllerTest.java
@@ -1,4 +1,23 @@
+import org.Mockito.Mock;
+import org.junit.jupiter.api.Test;
+import com.example.complain.repository.CategoryRepository;
+import org.Mockito.InjectMocks;
+
 @SpringBootTest
 public class CategoryControllerTest {
+    @InjectMocks
+    CategoryService categoryService;
 
+    @Mocks
+    CategoryRepository categoryRepository;
+
+    @Test
+    public void fetchAllCategory_should_return_status_200_when_success_find_all() {
+
+    }
+
+    @Test
+    public void fetchAllCategory_should_return_status_500_when_failed_to_find_all() {
+
+    }
 }
