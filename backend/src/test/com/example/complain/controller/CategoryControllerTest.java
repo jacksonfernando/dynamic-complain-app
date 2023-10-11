eb6eb3478b4d444a3cee1efb9a58466d00533752
jacksonfernando <jacksonfernando51@gmail.com>
2023-10-11 23:22:47 +0700

    finish writing test case description for categorycontroller

--- backend/src/test/com/example/complain/controller/CategoryControllerTest.java
+++ backend/src/test/com/example/complain/controller/CategoryControllerTest.java
@@ -30,4 +30,29 @@ public class CategoryControllerTest {
     public void fetchAllCategoryWithoutPagination_should_return_status_500_when_failed_to_find_all_without_pagination() {
 
     }
+
+    @Test
+    public void fetchCategoryById_should_return_status_200_when_success_fetch_by_id() {
+
+    }
+
+    @Test
+    public void saveCategory_should_return_status_201_when_success_saving_requets() {
+
+    }
+
+    @Test
+    public void saveCategory_should_return_status_500_when_success_saving_requets() {
+
+    }
+
+    @Test
+    public void deleteCategory_should_return_status_200_when_success_delete() {
+
+    }
+
+    @Test
+    public void deleteCategory_should_return_status_500_when_failing_delete() {
+
+    }
 }
