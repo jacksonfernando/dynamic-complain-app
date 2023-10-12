3d76fa47c4205eb23fb88d95784b3711a1478298
jacksonfernando <jacksonfernando51@gmail.com>
2023-10-12 21:31:33 +0700

    setup mockmvc

--- backend/src/test/com/example/complain/controller/CategoryControllerTest.java
+++ backend/src/test/com/example/complain/controller/CategoryControllerTest.java
@@ -1,15 +1,36 @@
 import org.Mockito.Mock;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
+
+import com.auth0.jwt.internal.com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.complain.controller.CategoryController;
+import com.example.complain.entity.Category;
 import com.example.complain.repository.CategoryRepository;
+import com.example.complain.service.CategoryService;
+
 import org.Mockito.InjectMocks;
 
 @SpringBootTest
 public class CategoryControllerTest {
+    private Category category = new Category(1, "title", "title", "multifile");
+
+    private MockMvc mockMvc;
+
     @InjectMocks
-    CategoryService categoryService;
+    CategoryController categoryController;
+
+    @Autowired
+    ObjecctMapper objectMapper;
 
     @Mocks
     CategoryRepository categoryRepository;
+
+    @BeforeEach
+    public void setup() {
+        objectMapper = new ObjectMapper();
+        mockMvc = MockMvcBuilders.standaloneSetup(this.categoryController);
+    }
 
     @Test
     public void fetchAllCategory_should_return_status_200_when_success_find_all() {
@@ -38,7 +59,7 @@ public class CategoryControllerTest {
 
     @Test
     public void saveCategory_should_return_status_201_when_success_saving_requets() {
-
+        when(categoryService.save(null)).thenReturn();
     }
 
     @Test
